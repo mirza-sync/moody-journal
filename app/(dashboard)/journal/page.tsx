24e384eef2d809c307cbd270702a5692d0f5112a
mirza-sync <mirzasyahmi123@gmail.com>
2024-02-18 18:53:52 +0800

    Query journal entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 
-function JournalPage() {
+const getEntries = async () => {
+  const user = await getUserByClerkID()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('entries', entries)
+
   return <div>Journal Page</div>
 }
 
