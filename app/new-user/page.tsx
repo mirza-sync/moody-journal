6de486791652ab2ddcbccdfa29f2d2763e35305c
mirza-sync <mirzasyahmi123@gmail.com>
2024-02-18 16:10:05 +0800

    Save new clerk user to db

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,33 @@
-const NewUserPage = () => {
-  return <div>NewUserPage</div>
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+const createNewUser = async () => {
+  const user = await currentUser()
+  console.log('Current user:', user)
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id,
+    },
+  })
+  console.log('Existing user:', existingUser)
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        clerkId: user!.id,
+        email: user!.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('/journal')
+}
+
+const NewUserPage = async () => {
+  await createNewUser()
+  return <div>new-user</div>
 }
 
 export default NewUserPage
