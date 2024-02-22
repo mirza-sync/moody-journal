1bf98b7c1bdc742bb9ca07ff0cbbf7f5220fb402
mirza-sync <mirzasyahmi123@gmail.com>
2024-02-23 00:36:35 +0800

    Feat: Update entry + autosave

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { updateEntry } from '@/utils/api'
 import { JournalEntry } from '@prisma/client'
 import { useState } from 'react'
+import { useAutosave } from 'react-autosave'
 
 type EditorProps = {
   entry: JournalEntry
@@ -9,8 +11,19 @@ type EditorProps = {
 
 const Editor = ({ entry }: EditorProps) => {
   const [value, setValue] = useState(entry.content)
+  const [isSaving, setIsSaving] = useState(false)
+  useAutosave({
+    data: value,
+    onSave: async (_value: string) => {
+      setIsSaving(true)
+      const updatedEntry = await updateEntry(entry.id, _value)
+      setIsSaving(false)
+    },
+  })
+
   return (
     <div className="h-full w-full">
+      {isSaving && <div>Saving...</div>}
       <textarea
         className="h-full w-full p-8 text-xl outline-none"
         value={value}
